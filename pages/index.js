2559961056cbf02c4139ba0cc56e35f2cdedaf00
Andrews_Gabriel <andrewsgabriel205@gmail.com>
2025-11-27 15:57:45 -0300

    modificação da frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 function Home () {
-    return <h1>DevComm is my personal project focus on replicanting Tab News</h1>
+    return <h1>DevComm is my personal project focus
+         on replicanting Tab News xD</h1>
 }
 
 export default Home;
