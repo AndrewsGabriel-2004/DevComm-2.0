c87bcda60b2832d07237651b27abe74f8117f003
Andrews_Gabriel <andrewsgabriel205@gmail.com>
2025-12-02 13:03:50 -0300

    Adiciona Scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # DevComm-2.0
+
 My own TabNews.com.br clone made on Curso.dev by Felipe Deschamps
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-function Home () {
-  return <h1>DevComm is my personal project focuson replicanting Tab 
-    News xD</h1>
+function Home() {
+  return (
+    <h1>DevComm is my personal project focuson replicanting Tab News xD</h1>
+  );
 }
 
 export default Home;
