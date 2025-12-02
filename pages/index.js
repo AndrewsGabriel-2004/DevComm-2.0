3eab9dbc7985a2aa94f6f1b36e80518830f7c403
Andrews_Gabriel <andrewsgabriel205@gmail.com>
2025-12-02 10:12:18 -0300

    adiciona arquivo `editorconfig`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home () {
-    return <h1>DevComm is my personal project focus
-         on replicanting Tab News xD</h1>
+  return <h1>DevComm is my personal project focuson replicanting Tab 
+    News xD</h1>
 }
 
 export default Home;
